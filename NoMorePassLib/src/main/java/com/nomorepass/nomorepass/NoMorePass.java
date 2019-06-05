ae22b168f48f53b385c1d072a05f094219afa322
becario <becario@verne>
2019-06-05 16:09:33 +0200

    Inicio

--- NoMorePassLib/src/main/java/com/nomorepass/nomorepass/NoMorePass.java
+++ NoMorePassLib/src/main/java/com/nomorepass/nomorepass/NoMorePass.java
@@ -27,18 +27,29 @@ public class NoMorePass {
     private String recuperaTicket(String json) {
         JsonParser parser = new JsonParser();
         JsonElement elementObject = parser.parse(json);
-        String ticket = elementObject.getAsJsonObject().get("ticket").getAsString();
-        return ticket;
+        String ret = elementObject.getAsJsonObject().get("ticket").getAsString();
+        return ret;
     }
 
     private String getData() {
         return "{'resultado':'ok','ticket':'qué coño pongo yo aquí'}";
     }
 
+    private String recuperaResultado(String json) {
+        JsonParser parser = new JsonParser();
+        JsonElement elementObject = parser.parse(json);
+        String ret = elementObject.getAsJsonObject().get("resultado").getAsString();
+        return ret;
+    }
+
     public String getQrText(String site) {
         String json = getData();
-        String tk = nmp_newtoken();
-        String ticket = recuperaTicket(json);
-        return "nomorepass://" + tk + ticket + site;
+        String resultado = recuperaResultado(json);
+        if (resultado.equals("ok")) {
+            String tk = nmp_newtoken();
+            String ticket = recuperaTicket(json);
+            return "nomorepass://" + tk + ticket + site;
+        }
+        return null;
     }
 }
