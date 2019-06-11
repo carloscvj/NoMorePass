0107e9026321e21110c324aef780620e523a5e27
becario <becario@verne>
2019-06-11 10:44:45 +0200

    Inicio

---
+++ NoMorePassGui/src/test/java/com/nomorepass/gui/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nomorepass.gui;
+
+import com.nomorepass.beans.NoMorePassBean;
+import com.nomorepass.gui.NoMorePassFrame;
+
+/**
+ *
+ * @author becario
+ */
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        NoMorePassFrame nmpf = new NoMorePassFrame();
+        NoMorePassBean nmpb = new NoMorePassBean();
+        nmpf.setBean(nmpb);
+        nmpf.setVisible(true);
+    }
+}
